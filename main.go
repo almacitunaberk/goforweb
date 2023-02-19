18d49e4c89ea8c44518f9b8dcbe9c80eb0ec3b54
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-02-20 02:46:34 +0300

    Initial cleaning

--- main.go
+++ main.go
@@ -5,14 +5,32 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "Hello World")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Bytes written is %d\n", n));
-	});
+const PORT = ":8080"
 
-	 http.ListenAndServe(":8080", nil)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "This is home page");
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fmt.Sprintf("Bytes written to HTTP Response Writer was %d \n", n));
+}
+
+
+func About(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "This is about page");
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fmt.Sprintf("Bytes written to HTTP Response Writer was %d \n", n));
+}
+
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Server listening on port %s", PORT))
+
+	 _ = http.ListenAndServe(PORT, nil);
 }
