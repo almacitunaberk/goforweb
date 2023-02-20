f780d83755c941b898f91fc7e2cdce739f1f4070
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-02-20 03:09:40 +0300

    Some cleanup

--- main.go
+++ main.go
@@ -2,31 +2,12 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const PORT = ":8080"
 
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl);
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println(err)
-		return;
-	}
-
-}
 
 func main() {
 
