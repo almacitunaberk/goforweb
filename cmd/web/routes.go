83119cfe23ac2c61c018935cd005f203b9d15ca5
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-02-21 15:16:14 +0300

    Added Chi for better route management

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,14 +5,22 @@ import (
 
 	"github.com/almacitunaberk/goforweb/pkg/config"
 	"github.com/almacitunaberk/goforweb/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
+	/* How to do routing manage with PAT library
 	mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	*/
+	mux := chi.NewRouter()
 
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
