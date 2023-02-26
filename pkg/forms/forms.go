2fa8e711c685c7c9f5a6240cea3b60a16ed6f88c
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-02-27 01:20:12 +0300

    Basic server side form validation is added

--- pkg/forms/forms.go
+++ pkg/forms/forms.go
@@ -10,6 +10,10 @@ type Form struct {
 	Errors errors
 }
 
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
 func New(data url.Values) *Form {
 	return &Form{
 		data,
@@ -20,6 +24,7 @@ func New(data url.Values) *Form {
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
+		f.Errors.Add(field, "This field cannot be blank")
 		return false
 	}
 	return true
